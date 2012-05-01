0216dab93d8423009f59a91e8931954ceaa8f875
Bernd Ritter <commel@gmail.com>
2012-05-01 12:38:52 +0200

    Artikel dürfen nur noch von Admins aktiviert/deaktiviert/eingefroren/aufgetaut werden

--- app/models/article.rb
+++ app/models/article.rb
@@ -14,13 +14,28 @@ class Article
   field :ftp, :type => Boolean, :default => false
   field :nouploads, :type => Boolean, :default => false
   field :redirection
+  field :historical, :type => Boolean, :default => false
   
   belongs_to :author, :class_name => "User", :inverse_of => :articles
   embeds_many :onlineshops
+
+  embeds_one :crossover
+  embeds_one :wine
   
-  validates_presence_of :title
+  validates_presence_of :title  
   validates_presence_of :content
   validates_presence_of :author
   validates_presence_of :tags
+
+  validate :keep_frozen_article
+  validate :keep_disabled_article
+  
+  def keep_frozen_article
+    errors.add(:title, "is not allowed to change") if (frozen_changed? && author.present? && !author.roles.include?("admin"))
+  end
+  
+  def keep_disabled_article
+    errors.add(:title, "is not allowed to change") if (disabled_changed? && author.present? && !author.roles.include?("admin"))
+  end
   
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -22,4 +22,58 @@ describe Article do
     article.save.should be_false
   end
   
+  it "should not be updatable when frozen" do
+    article = build(:article, :frozen => true)
+    article.title = "test123"
+    article.save.should be_false
+  end
+
+  it "should not be updatable when disabled" do
+    article = build(:article, :disabled => true)
+    article.title = "lalala"
+    article.save.should be_false
+  end
+  
+  it "should be unfreezable for admins" do
+    article = build(:article, :frozen => true)
+    article.frozen = false
+    article.author = build(:user, :roles => ["admin"])
+    article.save.should be_true
+  end
+  
+  it "should be able to enable for admins" do
+    article = build(:article, :disabled => true)
+    article.disabled = false
+    article.author = build(:user, :roles => ["admin"])
+    article.save.should be_true
+  end  
+  
+  it "should require admin-role to unfreeze" do
+    article = build(:article, :frozen => true)
+    article.frozen = false
+    article.author = build(:user, :roles => ["rhababer"])
+    article.save.should be_false
+  end
+  
+  it "should require admin-role to enable" do
+    article = build(:article, :disabled => true)
+    article.disabled = false
+    article.author = build(:user, :roles => ["lala"])
+    article.save.should be_false
+  end  
+  
+  it "should require admin-roles to disable" do
+    article = build(:article, :disabled => false)
+    article.author = build(:user, :roles => ["lala"])
+    article.disabled = true
+    article.save.should be_false
+  end
+  
+  it "should require admin-roles to freeze" do
+    article = build(:article, :frozen => false)
+    article.author = build(:user, :roles => ["lala"])
+    article.frozen = true
+    article.save.should be_false    
+  end
+
 end
