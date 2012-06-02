ed51f2abf1d93b57052ff94041a527ee7acffe8e
commel <commel@gmail.com>
2012-06-02 13:56:09 +0200

    Rest-Verben implementiert für News

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -4,12 +4,37 @@ class NewsController < ApplicationController
   end
 
   def new
+      @news = News.new
   end
 
   def create
+    @news = News.new(params[:news])
+ 
+    respond_to do |format|
+      if @news.save
+        format.html  { redirect_to(@news, :notice => 'News wurde erstellt.') }
+      else
+        format.html  { render :action => "new" }
+      end
+    end    
   end
 
   def update
+    @old_news = News.find(params[:id])
+    @news = @old_news.clone
+        
+    @old_news.historical = true
+    @old_news.save
+    
+    @news.author = current_user
+    
+    respond_to do |format|
+    if @news.update_attributes(params[:news])
+      format.html  { redirect_to(@news, :notice => 'News wurde aktualisiert.') }
+    else
+      format.html  { render :action => "edit" }
+    end
+  end
   end
 
   def destroy
@@ -20,5 +45,6 @@ class NewsController < ApplicationController
   end
 
   def edit
+    @news = News.find_by_slug(params[:id])
   end
 end
