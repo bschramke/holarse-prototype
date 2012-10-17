6c6ec3df4fa3a698c7cdbeff9a68c3fe92a32366
Bernd Ritter <commel@gmail.com>
2012-10-17 12:18:17 +0200

    Benutzerprofil verbessert

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,6 +10,14 @@ module UsersHelper
       city
     end
   end
-  
+ 
+  # 
+  # zeigt den freischaltungsstatus
+  # eines minecraft-benutzers
+  # grafisch an.
+  #
+  def display_mc_whitelist_status(status)
+    status.present? && status ? image_tag("gtk-apply.svg", :alt => t('.whitelisted'), :class => "mc_whitelist_status") : image_tag("gtk-dialog-errors.svg", :alt => t('.not_whitelisted'), :class => "mc_whitelist_status")
+  end
 
 end
