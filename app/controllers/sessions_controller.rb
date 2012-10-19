05da2182c1c039e1e67f2efa65ad947bf238697e
Bernd Ritter <commel@gmail.com>
2012-10-19 16:51:55 +0200

    Entkopplung von Logik in eigene Klassen für Login-Prozess

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,36 +10,20 @@ class SessionsController < ApplicationController
     # benutzer-account erstmal finden
     user = User.where(:username => params[:username]).first        
 
-    # benutzer wurde nicht gefunden
-    if user == nil
-      Rails.logger.debug("User #{params[:username]} not found")
-      flash[:error] = "Benutzer konnte nicht gefunden werden"
-      return
-    end
-
-    # benutzer nicht aktiv    
-    if !user.active
-      Rails.logger.debug("User #{params[:username]} not active")
-      flash[:error] = "Benutzer ist deaktiviert."
-      return
-    end
-    
-    # zu viele fehlversuche
-    if user.failed_logins > 3
-     Rails.logger.debug("User #{params[:username]} has too many failed logins")        
-     flash[:error] = "Benutzer hat bereits zuviele Fehlversuche"
-     return
+    begin
+      UserValidator.new(user).is_valid?
+    rescue => e
+      flash[:error] = e.message
+      redirect_to root_path and return
     end
 
     # wenn der benutzer noch ein altes passwort hat,
     # dann die migration aufrufen
-    Rails.logger.debug("Checking for legacy password")
-    if user && user.active && user.old_password_hash
-      Rails.logger.debug("Legacy password found")
-      # den benutzer migrieren
-      do_migrate_user(user, params)
+    um = UserMigrator.new(user, params[:password])
+    if um.has_legacy_password? && um.is_authenticated_with_legacy_password?
+      flash[:notice] = "Der Account wurde migriert - und eingeloggt."
+      redirect_to root_path and return
     end
-    Rails.logger.debug("No Legacy password found")    
 
     # neue benutzer-authentifikation
     Rails.logger.debug("Trying to login")
@@ -53,16 +37,12 @@ class SessionsController < ApplicationController
       Rails.logger.debug("Login ok")      
       # zurück zur hauptseite
       flash[:notice] = "Der Login war erfolgreich."
-      return
+      redirect_to root_path and return
+    else
+      user.inc(:failed_logins, 1)
+      flash[:error] = "Login fehlgeschlagen"
+      redirect_to root_path and return
     end
-    
-    Rails.logger.debug("Login failed")
-    
-    # fehlgeschlagene versuche hochzählen
-    user.inc(:failed_logins, 1)
-     
-    # zurück zur hauptseite
-    flash[:error] = "Login fehlgeschlagen."
   end
   
   def show
@@ -79,23 +59,41 @@ class SessionsController < ApplicationController
     redirect_to root_url, :notice => 'Erfolgreich abgemeldet'  
   end
   
-  private
+end
+
+#
+# Validiert einen Benutzer
+#
+class UserValidator
+
+  def initialize(user)
+    @user = user
+  end
   
-  #
-  # migriere den benutzer auf das neue passwortsystem
-  #
-  def do_migrate_user(user, params)
-    Rails.logger.debug("Migrating #{user.username}")
-    # wenn es ein altes passwort gibt, dann verwende dieses, bis ein neues
-    # eingegeben wird.
-    if user && user.active && user.old_password_hash && user.authenticate_legacy(params[:password])
+  def is_valid?
+    raise StandardError, "Benutzer konnte nicht gefunden werden" if !@user.present?
+    raise StandardError, "Benutzer ist nicht aktiv" if !@user.active
+    raise StandardError, "Zuviele fehlgeschlagene Login-Versuche" if @user.failed_logins > 3
+  end
+  
+end
 
-      # wandle das alte passwort in das neue um
-      user.update_attributes(password: params[:password])
-      
-      # das alte passwort löschen
-      user.unset(:old_password_hash)
-    end  
-  end  
+class UserMigrator
+  def initialize(user, password)
+    @user = user
+    @password = password
+  end
 
+  def has_legacy_password?
+    @user.old_password_hash
+  end
+
+  def is_authenticated_with_legacy_password?
+    @user.authenticate_legacy(@password)
+  end
+
+  def migrate
+    @user.update_attributes(password: @password)
+    @user.unset(:old_password_hash)
+  end
 end
