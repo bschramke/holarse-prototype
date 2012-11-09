e3c5cbe02fe6651b6d13a3c03b672a15763b2df3
Bernd Ritter <commel@gmail.com>
2012-11-09 21:51:28 +0100

    Berechtigungen für Benutzer-Profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,17 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def is_logged_in?
+    current_user.present?
+  end
+
+  def has_role(rolename)
+    current_user.roles.include?(rolename.to_s)
+  end
   
-  helper_method :current_user
+  helper_method :current_user, :is_logged_in?
+
+
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  before_filter :require_view_permissions, :only => [:index, :show]
+  before_filter :require_edit_permissions, :only => [:edit, :update, :destroy]
+
   #
   # zeigt alle benutzer
   #
@@ -57,5 +60,40 @@ class UsersController < ApplicationController
   # ermögliche das bearbeiten eines benutzerprofils
   #
   def edit
+    @user = User.find params[:id]
   end
+
+  private
+
+  #
+  # man darf userseiten nur sehen, wenn man 
+  # 1. eingeloggt ist
+  #
+  def require_view_permissions
+    unless is_logged_in?
+      flash[:error] = "Seite nur sichtbar, wenn man eingeloggt ist."
+      redirect_to root_path
+    end 
+  end
+
+  #
+  # man darf userseiten nur bearbeiten, wenn man
+  # 1. eingeloggt ist
+  # 2. man in der rolle "administrator" ist
+  # 3. die anzuzeigende benutzerseite dem aktuell eingeloggtem benutzer entspricht
+  #
+  def require_edit_permissions
+    unless is_logged_in? && (has_role(:administrator) || accesses_own_profile)
+      flash[:error] = "Keine ausreichenden Rechte f&uuml;r diese Seite"
+      redirect_to root_path
+    end
+  end
+
+  #
+  # prueft, ob die aufzurufende seite, das eigene profil ist
+  #
+  def accesses_own_profile
+    params[:username] == current_user.username
+  end
+
 end
