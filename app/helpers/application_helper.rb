7b09553ffdc73f1ec3489185ca222e0425e64d78
Bernd Ritter <commel@gmail.com>
2012-11-12 23:22:11 +0100

    Dynamisches Wiki-Parsing-System

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,13 @@
+require "lib/wiki"
+
 module ApplicationHelper
 
   def get_app_version
     Holarse::Application.config.app_version
+  end
+
+  def wiki_to_html(text)
+    Holarse::Wiki::Parser.new(text).format_to_html
   end
 
 end
--- lib/wiki.rb
+++ lib/wiki.rb
@@ -1,28 +1,47 @@
 module Holarse
   module Wiki
+    
     class Parser
       include Rails.application.routes.url_helpers
 
-      @@mapping_table = {
-        /^=== (.+) ===$/    => "<h3>#{$1}</h3>",                                                   # heading 3
-        /^==== (.+) ====$/  => "<h4>#{$1}</h3>",                                                   # heading 4
-        /\[(.+) (.+)\]/     => "<a href=\"#{$1}\">#{$2}</a>",                                      # externer link
-        /\[\[(.+)\]\]/      => ActionController::Base.helpers.link_to("#{$1}", Rails.application.routes.url_helpers.articles_path("#{$1}".to_url)),
-        /^(?!\*).*((\n\*+.*)+)\n(?!\*).*/ =>  "<ul>#{$1}\n</ul>", # liste
-        /^(?!\*{2,}).*((\n\*{2,}.*)+)\n(?!\*{2,}).*/ =>  "<ul>#{$1}\n</ul>",  # liste 2
-        /^\*+(.*)/  => "<li>#{$1}</li>", # listelement
-        /\[code\](.*)\[\/code\]/ => "<div class=\"code\">#{$1}</div>" # code
-      } 
-
       def initialize(wikitext)
         @wikitext = wikitext
-        puts "#{@@mapping_table.inspect}"
       end
 
-      def to_html
-        @wikitext.gsub(Regexp.union(@@mapping_table.keys), @@mapping_table)
+      def format_to_html
+        txt = @wikitext
+        get_parse_types.each do |parsetype|
+          txt = send(parsetype, txt)
+        end
+        txt
       end
 
+      private
+
+      def get_parse_types
+        [:wiki_links, :external_links]
+      end
+
+      def plain(text)
+        text
+      end
+
+      def external_links(text)
+        text.gsub(/\[(.*) (.*)\]/) { |match|
+          ActionController::Base.helpers.link_to("{$2}", "${1}")
+        }
+     end
+
+      def wiki_links(text)
+        text.gsub(/\[\[([\w \|]+)\]\]/) { |match|
+          ActionController::Base.helpers.link_to("#{$1}", Rails.application.routes.url_helpers.articles_path("#{$1}".to_url) )
+        }
+      end
+
+      def bla(text)
+        "bla"
+      end
     end
+
   end
 end
