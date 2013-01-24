6242ac3b6f7500f5bae491ab221c3299d184c7f2
Moritz Kiefer <moritz.kiefer@gmail.com>
2013-01-24 21:31:56 +0100

    Links, Videos und Screenshots wieder funktionsfähig machen

--- app/models/news.rb
+++ app/models/news.rb
@@ -7,6 +7,14 @@ class News < ActiveRecord::Base
   has_many :links
   has_many :comments
 
+  #accepts_nested_attributes_for :screenshots, :reject_if => lambda { |a| a[:image].blank? }, :allow_destroy => true
+  #accepts_nested_attributes_for :videos, :reject_if => lambda { |a| a[:url].blank? }, :allow_destroy => true
+  #accepts_nested_attributes_for :links, :reject_if => lambda { |a| a[:url].blank? }, :allow_destroy => true
+
+  accepts_nested_attributes_for :screenshots
+  accepts_nested_attributes_for :videos
+  accepts_nested_attributes_for :links
+
   acts_as_taggable
 
   validates_presence_of :title  
