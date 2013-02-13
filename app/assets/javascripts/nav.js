8691261fb79ba1efcd44a800aff0afb6715af72c
Bernd Ritter <commel@gmail.com>
2013-02-13 21:29:08 +0100

    Neue-Inhalten-Button

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,4 +1,22 @@
-  $(function() {
+$(document).ready(function() {
+  // Neue-Inhalte-Button
+  $("#new_button").button({
+    icons: {
+      primary: "ui-icon-circle-plus",
+      secondary: "ui-icon-triangle-1-s"
+    }
+  }).click(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    $("#newbtn-content").dialog({
+      height: 140,
+      modal: true
+    });
+  });
+});  
+                                                        
+$(function() {
     window.scrolled = false;
     return $(window).scroll(function() {
       if ($(window).scrollTop() > 50) {
@@ -8,6 +26,11 @@
         });
         $("#smalltux").fadeIn('fast');
         $("#smalltuxtext").fadeIn('fast');
+        $("#new_button").animate({
+          position: 'fixed',
+          left: '225px',
+          bottom: '15px'
+        });
       } else {
         $(".secondary").css({
           position: 'absolute',
