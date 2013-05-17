f3e69c129391aa29b75622a35fe374b383b4b4f8
Bernd Ritter <commel@gmail.com>
2013-05-17 11:22:45 +0200

    Tests korrigiert

--- spec/features/home_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe 'home page' do
-    it 'should show the welcome page' do
-        visit '/'
-        page.should have_content('HelloWorld')
-    end
-end
--- spec/features/videos_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe 'videos page' do
-    it 'should show the youtube-videos page' do
-        visit '/videos'
-        page.should have_content('Videos')
-    end
-end
