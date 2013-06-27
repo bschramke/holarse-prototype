86e24db8353af3c416f9affe8bf32b3eb9365c29
Bernd Ritter <commel@gmail.com>
2013-06-28 00:16:34 +0200

    Null-Fehler im Link-Generator behoben + Refactoring

--- app/controllers/link_generator_controller.rb
+++ app/controllers/link_generator_controller.rb
@@ -4,7 +4,33 @@ class LinkGeneratorController < ApplicationController
 
   def index
     articles = Article.select("title, alternate_title, id").order(:title)
-    joined_res = articles.map { |o| { :id => o.id, :title => o.title } } + articles.delete_if { |i| i.alternate_title.nil? || i.alternate_title.empty? }.map { |i| { :id => i.id, :title => i.alternate_title} }
-    render :text => joined_res.to_json
+    render :text => create_title_list(articles).to_json
   end
+
+  private
+
+  def create_title_list(articles)
+    clean combine_titles(articles)
+  end
+
+  def combine_titles(articles)
+    articles.map(&to_title_objects) + articles.map(&to_alternate_title_objects)
+  end
+
+  def clean(articles)
+    articles.delete_if &misses_title
+  end
+
+  def to_title_objects
+    Proc.new {|o| { :id => o.id, :title => o.title } }
+  end
+
+  def to_alternate_title_objects
+    Proc.new {|o| { :id => o.id, :title => o.alternate_title } }
+  end
+
+  def misses_title
+    Proc.new {|i| i[:title].nil? || i[:title].empty? }
+  end
+
 end
