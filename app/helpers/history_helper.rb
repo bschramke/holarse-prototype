18072a0c08fc5f6f4b02b7c289925ba9b2a67bd3
Bernd Ritter <commel@gmail.com>
2013-06-28 00:30:36 +0200

    Revisionen als Tabelle

--- app/helpers/history_helper.rb
+++ app/helpers/history_helper.rb
@@ -1,2 +1,10 @@
 module HistoryHelper
+
+  #
+  # erzeugt einen link auf eine revision einer news oder eines artikels
+  #
+  def revision_link(parent, revision)
+    link_to revision.id, url_for(:controller => :history, :action => :show, "#{parent.class.name.underscore}_id".to_sym => parent.id, :id => revision)
+  end
+
 end
