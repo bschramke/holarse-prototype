81ed99d91707d0f1d1d1770848c77d2a9bf378ff
Bernd Ritter <comrad@holarse-linuxgaming.de>
2013-09-17 22:09:44 +0200

    Die Vorschaufunktion hat durch eine API-Abfrage ohne CSRF-Daten zu einem Schließen der Session geführt. Das sollte hiermit korrigiert sein.

--- app/controllers/markup_preview_controller.rb
+++ app/controllers/markup_preview_controller.rb
@@ -1,4 +1,7 @@
 class MarkupPreviewController < ApplicationController
+
+  skip_before_filter :verify_authenticity_token, :only => [:preview]  
+
   def preview
     render :text => Holarse::Markup.render(params[:content])
   end
