b948501f2cbae8e6e7da52469880e9ac2c6d6022
Bernd Ritter <comrad@holarse-linuxgaming.de>
2013-09-18 10:56:38 +0200

    Datum wird nun lokalisiert, Es wird eine Liste aller Autoren eines Artikels angezeigt, Die Formulare sind nun hübscher.

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,7 @@
 module ArticlesHelper
+
+  def article_authors(article)
+    article.versions.map { |version| version.whodunnit }.uniq.map { |userid| User.find userid }.sort { |x, y| x.username <=> y.username }
+  end
+
 end
