d7570e887bd2f1d4b7a7906572af6f688be117b8
Bernd Ritter <comrad@holarse-linuxgaming.de>
2013-12-09 10:30:08 +0100

    Änderungen aus Travis-Fails NewsKategorien korrigiert beim Test Konfigurationsvariablen angepasst. I18N auf Rails-Standard gesetzt. Whiny-Nils entfernt.

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ Role.create([{name: 'admin'},{name: 'reporter'}, {name: 'moderator'}])
 puts "#{Role.count} Rollen angelegt"
 
 NewsCategory.delete_all
-NewsCategory.create([{code: 'announcement', description: 'Ankündigung'}, {code: 'release', description: 'Veröffentlichung'}, {code: 'patch', description: "Patch"}, {code: 'video', description: 'Videonews'}, {code: 'audio', description: 'Podcast'}, {code: 'hardware', description: 'Hardware'}])
+NewsCategory.create([{name: 'announcement', description: 'Ankündigung'}, {name: 'release', description: 'Veröffentlichung'}, {name: 'patch', description: "Patch"}, {name: 'video', description: 'Videonews'}, {name: 'audio', description: 'Podcast'}, {name: 'hardware', description: 'Hardware'}])
 puts "#{NewsCategory.count} News-Kategorien angelegt"
 
 ProjectActivityState.delete_all
