a13e0ba4f7e758b1db563008a78b8d6322760fb1
Bernd Ritter <comrad@holarse-linuxgaming.de>
2014-01-15 21:30:30 +0100

    News-Updates können nun bearbeitet werden.

--- app/controllers/news_updates_controller.rb
+++ app/controllers/news_updates_controller.rb
@@ -3,16 +3,54 @@ class NewsUpdatesController < ApplicationController
 
   add_breadcrumb "News"
 
-  def new
-    @news = News.find(params[:news_id])
+  def index
+    @news = load_news
     add_breadcrumb @news.title, @news
+    add_breadcrumb "Updates"
+    add_breadcrumb "Übersicht"
+    @news_updates = @news.news_updates
+  end
+
+  def show
+    @news = load_news
+    add_breadcrumb @news.title, @news
+    add_breadcrumb "Updates"
+    @news_update = NewsUpdate.find(params[:id])
+    add_breadcrumb @news_update.id
+  end
+
+  def update
+    @news = load_news
+    @news_update = NewsUpdate.find(params[:id])
+    @news_update.user = current_user
+
+    @news_update.assign_attributes(news_update_params)
+
+    @news_update.save
+
+    redirect_to @news
+  end
+
+  def edit
+    @news = load_news
+    add_breadcrumb @news.title, @news
+    add_breadcrumb "Updates"
+    @news_update = NewsUpdate.find(params[:id])
+    add_breadcrumb @news_update.id
+    add_breadcrumb "Bearbeiten"
+  end
+
+  def new
+    @news = load_news
+    add_breadcrumb @news.title, @news
+    add_breadcrumb "Updates"
     add_breadcrumb "Neues Update"
     @news_update = NewsUpdate.new
     render :edit
   end
 
   def create
-    @news = News.find(params[:news_id])
+    @news = load_news
     @news_update = NewsUpdate.new(news_update_params)
 
     @news_update.user = current_user
@@ -31,6 +69,10 @@ class NewsUpdatesController < ApplicationController
 
   def news_update_params
     params.require(:news_update).permit(:content)
+  end
+
+  def load_news
+    News.find(params[:news_id]).decorate
   end
 
 end
