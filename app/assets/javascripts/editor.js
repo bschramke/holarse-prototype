946d19a11d8909557200e429037afd5de0fba1a0
Bernd Ritter <comrad@holarse-linuxgaming.de>
2014-02-25 22:32:29 +0100

    Toggle-Buttons für Live-Vorschau und Kurz-/Lang-News

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -1,12 +1,42 @@
 $(document).ready(function() {
-  $("#toggle-edit").click(function(event) {
-    $("#livepreview").animate({"width": [ "toggle", "swing" ]}, 500, function() {
-      var visible = $("#livepreview").is(":visible");
-      if (visible) { 
-	$("#input").removeClass("col-md-12").addClass("col-md-6");
-      } else {
-	$("#input").removeClass("col-md-6").addClass("col-md-12");
-      }
-    });
+
+  $("#toggle-livepreview").click(function(event) { 
+    $("#livepreview").toggle();
+
+    var is_preview_visible = $("#livepreview").is(":visible");
+
+    if (is_preview_visible) {
+      $("#input").removeClass("col-md-12").addClass("col-md-6");
+    } else {
+      $("#input").removeClass("col-md-6").addClass("col-md-12");
+    }
   });
+
+  $("#toggle-newsview").click(function(event) {
+    var newsview = $(this).data("newsview");
+    var toggled = holarse.toggle_newsview_value(newsview);
+
+    $("#news_subtitle_input").toggle();
+    $("#news_news_category_input").toggle();
+    $("#news_category_list_input").toggle();
+
+    if (newsview === "long") {
+      $("#markup-edit-content").css("height", "200px");
+      $("#toggle-livepreview").show();
+      $("#livepreview").show();
+      $("#input").removeClass("col-md-12").addClass("col-md-6");
+    } else {
+      $("#markup-edit-content").css("height", "80px");
+      $("#toggle-livepreview").hide();
+      $("#livepreview").hide();
+      $("#input").removeClass("col-md-6").addClass("col-md-12");
+    }
+
+    $(this).data("newsview", toggled);
+  });
+
+  holarse.toggle_newsview_value = function(newsview_value) {
+    return (newsview_value === "long" ? "short" : "long");
+  }
+
 });
