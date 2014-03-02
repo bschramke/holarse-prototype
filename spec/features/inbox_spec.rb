87016599f4b52c6a147ad024fb9576b304a64a05
Bernd Ritter <comrad@posteo.de>
2014-03-02 19:36:15 +0000

    Tests für das Inbox-Feature

--- spec/features/inbox_spec.rb
+++ spec/features/inbox_spec.rb
@@ -9,7 +9,7 @@ describe "inbox pages", type: :feature do
   end
 end
 
-describe "Visitor wants to pass a new notification", type: :feature do
+describe "Visitor wants to write a new notification", type: :feature do
   it "is accessible on the root page" do
     visit root_path
     click_link "Neuigkeiten einreichen"
@@ -17,5 +17,39 @@ describe "Visitor wants to pass a new notification", type: :feature do
     page.should have_content "Neuigkeiten einreichen"
   end
 
+  it "should automatically fill the username if logged in" do
+    # login
+    user = Fabricate(:user)
+    visit login_path
+    within("#new_session") do
+      fill_in "Benutzername", with: user.username
+      fill_in "Passwort", with: user.password
+    end
+    click_button "Anmelden"
+    page.should have_content "Login war erfolgreich"
 
+    # new inbox
+    visit new_inbox_path
+    page.should have_field("Absender", with: user.username)
+  end
+
+  it "should save the inbox message" do
+    count_before = Inbox.count
+
+    inbox = Fabricate(:inbox)
+    visit new_inbox_path
+
+    Rails.logger.debug("inbox: #{inbox.inspect}")
+
+    within("#new_inbox") do
+      fill_in "Absender", with: inbox.sender
+      fill_in "Web-Adresse", with: inbox.url
+      fill_in "Info-Text", with: inbox.info
+    end
+
+    click_button "Einreichen"
+    page.should have_content "Aktuelle Einreichungen"
+
+    expect(Inbox.count).to eq(count_before + 1)
+  end
 end
